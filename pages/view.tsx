2859caf846d8c857729397c4c4fe4ece8ab41b8e
paragkoche <paragkoche0@gmail.com>
2022-01-08 20:55:13 +0530

    next

---
+++ pages/view.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next"
+const view: NextPage = () => {
+    return (
+        <main className=" flex w-full h-[88%] mt-20 ">
+            <main className=" mr-[220px] w-[75%">
+                <section>
+                    <div className=" flex flex-col max-5">
+
+                    </div>
+                </section>
+            </main>
+            <section>
+
+            </section>
+        </main>
+    )
+}
+export default view
